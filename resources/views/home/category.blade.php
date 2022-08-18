2617925954f27f80790e231d56fbb136b9647e1f
Ibadd <muhammaditsfan@webmail.umm.ac.id>
2022-08-18 09:13:56 +0700

    fixing icons

--- resources/views/home/category.blade.php
+++ resources/views/home/category.blade.php
@@ -18,7 +18,7 @@
                 <a href="{{ route('proyek-desa-home') }}" class="text-1000 ">
                     <div class="card bg-400">
                         <div class="card-body pb-1 pt-1" style="position: relative;text-align: center;">
-                            <img src="https://tamiajeng.my.id/desa/themes/batuah_22_4_2/linkplus/icon/pembangunan.png" class="img-fluid fit-cover rounded-1 absolute-sm-centered pb-2" style="max-width: 50px" alt="">
+                            <img src="https://tamiajeng.desa.id/desa/themes/batuah_merdeka/images/iconsvg/pemb.svg" class="img-fluid fit-cover rounded-1 absolute-sm-centered pb-2" style="max-width: 50px" alt="">
                             <span class="fw-semi-bold" style="font-size: 12px;position: relative; display: flex;justify-content: center">Pembangunan</span>
                         </div>
                     </div>
